b45b18fcebe496485c5bb26fa405fadbbbde9ce7
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-07 12:17:02 +0100

    Build v 3.9.0; Allow to use mkl; fix C/CPP flags on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,27 +4,55 @@ set -x
 
 mkdir build && cd build
 
-#if [[ "$(echo $fortran_compiler_version | cut -d '.' -f 1)" -gt 9 ]]; then
-#  export FFLAGS="$FFLAGS -fallow-argument-mismatch"
-#fi
+if [[ "$(echo $fortran_compiler_version | cut -d '.' -f 1)" -gt 9 ]]; then
+  export FFLAGS="$FFLAGS -fallow-argument-mismatch"
+fi
+
+case "${blas_impl}" in
+        mkl)
+            export BLAS="MKL"
+            export USE_MKL=1
+            export USE_MKLDNN=1
+	    export BLA_VENDOR="Intel"
+            ;;
+        openblas)
+            export BLAS="OpenBLAS"
+            export USE_MKL=0
+            export USE_MKLDNN=0
+	    export BLA_VENDOR="OpenBLAS"
+            ;;
+        *)
+            echo "[ERROR] Unsupported BLAS implementation '${blas_impl}'" >&2
+            exit 1
+            ;;
+    esac
 
 if [[ ${HOST} =~ .*linux.* ]]; then
   # Need to point to libquadmath.so.0
   export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
 fi
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LIBS="$LIBS -framework Accelerate"
+  export FFLAGS="$FFLAGS -ff2c -fno-second-underscore"
+  export FCFLAGS="$FCFLAGS -ff2c -fno-second-underscore"
+fi
+
 for shared_libs in OFF ON
 do
   cmake ${CMAKE_ARGS} \
-    -DCMAKE_Fortran_FLAGS="-fallow-argument-mismatch" \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_SHARED_LIBS=${shared_libs} \
-    -DBLAS_LIBRARIES="-lopenblas" \
     -DICB=ON \
     -DMPI=${DMPI} \
     ..
+
+  if [[ ${mpi} != 'nompi' ]]; then
+    sed -i '' "s/-fallow-argument-mismatch//g" $SRC_DIR/build/CMakeFiles/icb_parpack_cpp.dir/flags.make
+    sed -i '' "s/-fallow-argument-mismatch//g" $SRC_DIR/build/CMakeFiles/icb_parpack_c.dir/flags.make
+  fi
   make install -j${CPU_COUNT} VERBOSE=1
 done
 
